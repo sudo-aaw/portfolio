6947ff364550fa0e866cec78b874157632935be9
arch <archiejaikenwood@gmail.com>
2024-09-03 14:04:36 +0100

    centralised the archie tile pattern

--- my-app/src/lib/components/pathFinding/TileGrid.svelte
+++ my-app/src/lib/components/pathFinding/TileGrid.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 
- 
-    
     const tileSize = 40;
-
-
-    let anchorTile = [10,0];
+    let anchorTile = [0,0];
     let activeNumber = [0,0];
 
     let innerWidth = 0
     let innerHeight = 0
 
+    const archieTilesPattern = [[1,4],[1,3], [1,2], [1,1], [1,0], [2,0], [3,0], [3,1], [3,2], [3,3],[3,4],[2,2], [5,4], [5,3], [5,2], [5,1], [5,0], [6,0], [7,1], [6,2], [7,3], [7,4],[10,0], [9,0], [9,1], [9,2], [9,3], [9,4], [10,4], [11,4], [11,0], [13,4], [13,3], [13,2], [13,1], [13,0], [14,2], [15,0], [15,1], [15,2], [15,3],[15,4],[17,4], [18,4], [19,4], [18,3], [18,2], [18,1], [17,0], [18,0], [19,0],[21,4], [22,4], [21,3], [21,2], [22,2], [21,0], [22,0], [21,1], [23,4], [23,2],[23,0]];
 
-        
-    $: archieTiles = new Set(["1,4","1,3", "1,2", "1,1", "1,0", "2,0", "3,0", "3,1", "3,2", "3,3","3,4","2,2", "5,4", "5,3", "5,2", "5,1", "5,0", "6,0", "7,1", "6,2", "7,3", "7,4","10,0", "9,0", "9,1", "9,2", "9,3", "9,4", "10,4", "11,4", "11,0", "13,4", "13,3", "13,2", "13,1", "13,0", "14,2", "15,0", "15,1", "15,2", "15,3","15,4","17,4", "18,4", "19,4", "18,3", "18,2", "18,1", "17,0", "18,0", "19,0","21,4", "22,4", "21,3", "21,2", "22,2", "21,0", "22,0", "21,1", "23,4", "23,2","23,0"]);
+
+    $: archieTiles = new Set();
+    let archieXTiles = 25;
+    let archieYTiles = 7;
 
     $:xTiles = Math.floor(innerWidth/tileSize);
     $:yTiles = Math.floor(innerHeight/tileSize);
+
+    $:archieStartX = (xTiles/2)-archieXTiles/2;
+    $:archieStartY = (yTiles/2)+archieYTiles/2;
 
     $:containerWidth = xTiles*tileSize;
     $:containerHeight = yTiles*tileSize;
@@ -58,11 +60,25 @@
         var coordAsString = coord[0] + "," + coord[1];
         return archieTiles.has(coordAsString);
     }
+
+    onMount(()=>{
+        let newSet = new Set();
+        archieTilesPattern.forEach(tile => {
+            
+            let x = Math.floor(tile[0]-archieStartX);
+            let y = Math.floor(tile[1]+archieStartY);
+            let coord = x+","+y;
+            console.log(x)
+            newSet.add(coord);
+        });
+        archieTiles=newSet;
+    })
+    console.log(archieStartX)
+    console.log(archieStartY)
+    
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight />
-
-
 
 <div class={containerClass}>
     {#each Array(yTiles) as _, y}
